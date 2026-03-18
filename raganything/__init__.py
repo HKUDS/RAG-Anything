93413a7d40c3453d01bdadec0ba934188dd333a3
minhnion <nguyenminh2k4pchy@gmail.com>
2026-03-18 23:02:17 +0700

    release: prepare gsk-raganything 1.3.0

--- raganything/__init__.py
+++ raganything/__init__.py
@@ -1,8 +1,8 @@
 from .raganything import RAGAnything as RAGAnything
 from .config import RAGAnythingConfig as RAGAnythingConfig
 
-__version__ = "1.2.8"
+__version__ = "1.3.0"
 __author__ = "Zirui Guo"
-__url__ = "https://github.com/HKUDS/RAG-Anything"
+__url__ = "https://github.com/minhnion/RAG-Anything"
 
 __all__ = ["RAGAnything", "RAGAnythingConfig"]
